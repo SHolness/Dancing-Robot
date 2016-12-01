f2228ca5a87cf4915073f940b14e3f905ba12d34
piscetellil <pisceetellil@rider.edu>
2016-12-01 10:55:46 -0500

    move

---
+++ driveme4/driveme4.ino
@@ -0,0 +1,18 @@
+#include <RedBot.h>
+RedBotMotors motors;
+RedBotEncoder encoder = RedBotEncoder(A2, 10);
+int buttonPin = 12;
+int countsPerRev = 192;
+float wheelDiam = 2.56;  
+float wheelCirc = PI*wheelDiam;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ driveDistance(12, 150);
+  turnRight();
+}
