554492ef292403fe8634513b8828db30bb4b7808
Shayna Holness <holnesss@rider.edu>
2016-12-01 11:09:46 -0500

    jingle bells!

--- Robot_Music/Robot_Music.ino
+++ Robot_Music/Robot_Music.ino
@@ -23,76 +23,87 @@ void loop() {
 
 void playDanceMusic()
 {
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(Rest, QN)
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(Rest, QN);
 
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(Rest, QN)
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(Rest, QN);
 
-  playNote(noteB4, QN)
-  playNote(noteD5, QN)
-  playNote(noteG4, QN+EN)
-  playNote(noteA4, EN)
+  playNote(noteB4, QN);
+  playNote(noteD5, QN);
+  playNote(noteG4, QN+EN);
+  playNote(noteA4, EN);
 
-  playNote(noteB4, HN+QN)
-  playNote(Rest, QN)
+  playNote(noteB4, HN+QN);
+  playNote(Rest, QN);
 
-  playNote(noteC5, QN)
-  playNote(noteC5, QN)
-  playNote(noteC5, QN+EN)
-  playNote(noteC5, EN)
+  playNote(noteC5, QN);
+  playNote(noteC5, QN);
+  playNote(noteC5, QN+EN);
+  playNote(noteC5, EN);
 
-  playNote(noteC5, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, EN)
-  playNote(noteB4, EN)
+  playNote(noteC5, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, EN);
+  playNote(noteB4, EN);
 
-  playNote(noteB4, QN)
-  playNote(noteA4, QN)
-  playNote(noteA4, QN)
-  playNote(noteB4, QN)
+  playNote(noteB4, QN);
+  playNote(noteA4, QN);
+  playNote(noteA4, QN);
+  playNote(noteB4, QN);
 
-  playNote(noteA4, HN)
-  playNote(noteC4, QN)
-  playNote(Rest, QN)
+  playNote(noteA4, HN);
+  playNote(noteD5, QN);
+  playNote(Rest, QN);
 
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(Rest, QN)
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(Rest, QN);
 
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(Rest, QN)
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(Rest, QN);
 
-  playNote(noteB4, QN)
-  playNote(noteD5, QN)
-  playNote(noteG4, QN+EN)
-  playNote(noteA4, EN)
+  playNote(noteB4, QN);
+  playNote(noteD5, QN);
+  playNote(noteG4, QN+EN);
+  playNote(noteA4, EN);
 
-  playNote(noteB4, HN+QN)
-  playNote(Rest, QN)
+  playNote(noteB4, HN+QN);
+  playNote(Rest, QN);
 
-  playNote(noteC5, QN)
-  playNote(noteC5, QN)
-  playNote(noteC5, QN+EN)
-  playNote(noteC5, EN)
+  playNote(noteC5, QN);
+  playNote(noteC5, QN);
+  playNote(noteC5, QN+EN);
+  playNote(noteC5, EN);
 
-  playNote(noteC5, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, QN)
-  playNote(noteB4, EN)
-  playNote(noteB4, EN)
+  playNote(noteC5, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, QN);
+  playNote(noteB4, EN);
+  playNote(noteB4, EN);
 
-  playNote(noteD5, QN)
-  playNote(noteD5, QN)
-  playNote(noteC5, QN)
+  playNote(noteD5, QN);
+  playNote(noteD5, QN);
+  playNote(noteC5, QN);
+  playNote(noteA4, QN);
+
+  playNote(noteG4, WN);
   
 }
 
+void playNote(int note, int duration)
+// This custom function takes two parameters, note and duration to make playing songs easier.
+// Each of the notes have been #defined in the notes.h file. The notes are broken down by 
+// octave and sharp (s) / flat (b).
+{
+  tone(buzzerPin, note, duration);
+  delay(duration);
+}
